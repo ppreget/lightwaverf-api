86cfeec65ee5bd50e72d14f2cb3c14120774bea2
Philippe Preget <philippepreget@Admins-MacBook-Pro-9.local>
2018-11-02 18:10:02 +0000

    wip

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+
+	"github.com/jimjibone/lightwavego"
+)
 
 func main() {
 
-	log.Println("Hello World")
-	//log.Println("another line")
+	log.Println("LightwaveRF API")
+	defer lightwavego.Shutdown()
 
+	lightOn, _ := lightwavego.NewBuffer([]byte{0x9, 0xf, 0x3, 0x1, 0x5, 0x9, 0x3, 0x0, 0x1, 0x2})
 
+	lwtx := lightwavego.NewLwTx()
+
+	lwtx.SendBuffer(lightOn)
+	time.Sleep(4 * time.Second)
 }
